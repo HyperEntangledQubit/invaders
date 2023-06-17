be683496cbf78aaa6fa0928511b78147f36022bb
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-06-16 21:15:38 -0400

    Searching for audio files to add into game

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[allow(unused_imports)]
+
+use glob::glob;
+// use std::error::Error;
+use rusty_audio::Audio;
+
+fn main(){// -> Result <(), Box<dyn Error>> {
+    let mut _audio = Audio::new();
+
+    for audiofile in glob("audio/*.wav").expect("Failed to find wav file") {
+        match audiofile {
+            Ok(path) => println!("{:?}", path.display()),
+            Err(e) => println!("{:?}", e),
+        }
+    }
 }
