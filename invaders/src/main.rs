f6ee07e083cae664110bde87048b49fde0bdcaa6
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-06-16 21:42:19 -0400

    Added audio files.

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -1,16 +1,33 @@
 #[allow(unused_imports)]
 
-use glob::glob;
-// use std::error::Error;
+// use glob::glob;
+use std::error::Error;
 use rusty_audio::Audio;
 
 fn main(){// -> Result <(), Box<dyn Error>> {
-    let mut _audio = Audio::new();
+    let mut audio = Audio::new();
 
-    for audiofile in glob("audio/*.wav").expect("Failed to find wav file") {
-        match audiofile {
-            Ok(path) => println!("{:?}", path.display()),
-            Err(e) => println!("{:?}", e),
-        }
-    }
+    // Replace these files with the actual sounds
+    audio.add("explode", "audio/explode.wav");
+    audio.add("lose", "audio/lose.wav");
+    audio.add("move", "audio/move.wav");
+    audio.add("pew", "audio/pew.wav");
+    audio.add("startup", "audio/startup.wav");
+    audio.add("win", "audio/win.wav");
+
+    // Using glob package to find files
+    // for audiofile in glob("audio/*.wav").expect("Failed to find wav file") {
+    //     match audiofile {
+    //         Ok(path) => audio.add(
+    //             path.file_stem().unwrap(),
+    //             path.file_name().unwrap()
+    //         ),
+
+    //         Err(e) => println!("{:?}", e),
+    //     }
+    // }
+    audio.play("startup");
+
+    // cleanup
+    audio.wait();
 }
